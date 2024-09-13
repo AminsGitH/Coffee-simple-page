683483233e472d43b9d325ebc3f08728a9ad9c6b
Amin Mohamed <aminjoker956@gmail.com>
2024-09-13 22:53:39 +0300

    s

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -12,7 +12,7 @@ const Product = (props) => {
         {props.rating ? (
           <img
             src="/dccl--frontend-simple-coffee-listing/Star_fill.svg"
-            alt=""
+            alt="rating"
           />
         ) : (
           <span className="text-light">No Ratings Available</span>

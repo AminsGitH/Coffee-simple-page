ee03c8a13e140d0228a1805d5118f355563feca6
Amin Mohamed <aminjoker956@gmail.com>
2024-09-13 22:51:28 +0300

    s

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,19 +1,27 @@
 const Product = (props) => {
   return (
-    <div className="product">
-          {props.popular && <p className="popular">Popular</p>}
-          <img src={props.image} alt="" className="drink-image card-img-top" />
-          <div className="d-flex justify-content-between"><h1 className="drink-h1">{props.name}</h1>
-              <button className="btn price-btn">{props.price}</button>
-          </div>
-          
-          <div className="">{props.rating ? <img src="/dccl--frontend-simple-coffee-listing/Star_fill.svg" alt="" /> :
-          <span className="text-light">No Ratings Available</span>}
-              <span className="drink-rating">{props.rating}</span></div>
-          
-          <span className="drink-votes">({props.votes} votes)</span>
-          {props.available ? null : <span className="sold">Sold Out</span>}
-          
+    <div className="product mb-5">
+      {props.popular && <p className="popular">Popular</p>}
+      <img src={props.image} alt="" className="drink-image card-img-top" />
+      <div className="d-flex justify-content-between">
+        <h1 className="drink-h1">{props.name}</h1>
+        <button className="btn price-btn">{props.price}</button>
+      </div>
+
+      <div className="">
+        {props.rating ? (
+          <img
+            src="/dccl--frontend-simple-coffee-listing/Star_fill.svg"
+            alt=""
+          />
+        ) : (
+          <span className="text-light">No Ratings Available</span>
+        )}
+        <span className="drink-rating">{props.rating}</span>
+      </div>
+
+      <span className="drink-votes">({props.votes} votes)</span>
+      {props.available ? null : <span className="sold">Sold Out</span>}
     </div>
   );
 };
